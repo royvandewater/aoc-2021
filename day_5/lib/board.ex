215226fda9b0202c991c3a1ac91346536a875204
Roy van de Water <roy.v.water@gmail.com>
2024-06-05 17:29:07 -0400

    We don't need bounds

--- day_5/lib/board.ex
+++ day_5/lib/board.ex
@@ -1,41 +1,24 @@
 defmodule Board do
-  def new(), do: %{bounds: %{x: 0..0, y: 0..0}, cells: %{}}
+  def new(), do: %{}
 
-  def add(board, line) do
-    board
-    |> Map.update!(:cells, &add_line(&1, line))
-    |> Map.update!(:bounds, &calculate_new_bounds(&1, line))
-  end
-
-  defp add_line(cells, {{x0, y}, {x1, y}}) do
+  def add(board, {{x0, y}, {x1, y}}) do
     x0..x1
     |> Enum.map(&{&1, y})
-    |> Enum.reduce(cells, &add_cell/2)
+    |> Enum.reduce(board, &add_cell/2)
   end
 
-  defp add_line(cells, {{x, y0}, {x, y1}}) do
+  def add(board, {{x, y0}, {x, y1}}) do
     y0..y1
     |> Enum.map(&{x, &1})
-    |> Enum.reduce(cells, &add_cell/2)
+    |> Enum.reduce(board, &add_cell/2)
   end
 
-  defp add_cell(cell, cells) do
-    Map.update(cells, cell, 1, &(&1 + 1))
-  end
-
-  defp calculate_new_bounds(bounds, {{x0, y0}, {x1, y1}}) do
-    minX = Enum.min([Enum.min(bounds[:x]), x0, x1])
-    maxX = Enum.max([Enum.max(bounds[:x]), x0, x1])
-
-    minY = Enum.min([Enum.min(bounds[:y]), y0, y1])
-    maxY = Enum.max([Enum.max(bounds[:y]), y0, y1])
-
-    %{x: minX..maxX, y: minY..maxY}
+  defp add_cell(cell, board) do
+    Map.update(board, cell, 1, &(&1 + 1))
   end
 
   def score(board) do
     board
-    |> Map.fetch!(:cells)
     |> Map.values()
     |> Enum.filter(&(1 < &1))
     |> Enum.count()
--- day_5/test/board_test.exs
+++ day_5/test/board_test.exs
@@ -4,48 +4,33 @@ defmodule Day5Test do
 
   describe "new" do
     test "should construct a new board" do
-      assert Board.new() == %{
-               bounds: %{x: 0..0, y: 0..0},
-               cells: %{}
-             }
+      assert Board.new() == %{}
     end
   end
 
   describe "add" do
     test "when called with a line segment that starts and ends on the same point" do
-      board = Board.new()
+      board = Board.new() |> Board.add({{0, 0}, {0, 0}})
 
-      assert Board.add(board, {{0, 0}, {0, 0}}) == %{
-               bounds: %{x: 0..0, y: 0..0},
-               cells: %{{0, 0} => 1}
-             }
+      assert board == %{{0, 0} => 1}
     end
 
     test "when called with a horizontal line segment" do
-      board = Board.new()
+      board = Board.new() |> Board.add({{0, 0}, {2, 0}})
 
-      assert Board.add(board, {{0, 0}, {2, 0}}) == %{
-               bounds: %{x: 0..2, y: 0..0},
-               cells: %{{0, 0} => 1, {1, 0} => 1, {2, 0} => 1}
-             }
+      assert board == %{{0, 0} => 1, {1, 0} => 1, {2, 0} => 1}
     end
 
     test "when called with a vertical line segment" do
-      board = Board.new()
+      board = Board.new() |> Board.add({{0, 0}, {0, 2}})
 
-      assert Board.add(board, {{0, 0}, {0, 2}}) == %{
-               bounds: %{x: 0..0, y: 0..2},
-               cells: %{{0, 0} => 1, {0, 1} => 1, {0, 2} => 1}
-             }
+      assert board == %{{0, 0} => 1, {0, 1} => 1, {0, 2} => 1}
     end
 
     test "when called with two line segments that overlap" do
       board = Board.new() |> Board.add({{0, 0}, {2, 0}}) |> Board.add({{0, 0}, {0, 2}})
 
-      assert board == %{
-               bounds: %{x: 0..2, y: 0..2},
-               cells: %{{0, 0} => 2, {1, 0} => 1, {2, 0} => 1, {0, 1} => 1, {0, 2} => 1}
-             }
+      assert board == %{{0, 0} => 2, {1, 0} => 1, {2, 0} => 1, {0, 1} => 1, {0, 2} => 1}
     end
   end
 
