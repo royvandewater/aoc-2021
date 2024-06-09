91409aafd935398d8b9ba38d5e98c17cd8d9c121
Roy van de Water <roy.v.water@gmail.com>
2024-06-09 12:44:50 -0400

    Day 7, part 2

--- day_7/lib/day7.ex
+++ day_7/lib/day7.ex
@@ -4,6 +4,7 @@ defmodule Day7 do
   def start(_type, _args) do
     input = File.read!("./input.txt") |> parse!()
     IO.puts("part 1: #{part_1(input)}")
+    IO.puts("part 2: #{part_2(input)}")
 
     {:ok, self()}
   end
@@ -16,31 +17,50 @@ defmodule Day7 do
   end
 
   def part_1(positions) do
-    gradient_descent(positions)
+    gradient_descent(&flat_cost/2, positions)
   end
 
-  defp gradient_descent(positions) do
+  def part_2(positions) do
+    gradient_descent(&triangle_cost/2, positions)
+  end
+
+  defp gradient_descent(cost_fn, positions) do
     min = Enum.min(positions)
     max = Enum.max(positions)
     curr = min + div(max - min, 2)
-    gradient_descent(positions, min, max, curr)
+    gradient_descent(cost_fn, positions, min, max, curr)
   end
 
-  defp gradient_descent(positions, min, max, curr) do
-    cost0 = cost(positions, curr - 1)
-    cost1 = cost(positions, curr)
-    cost2 = cost(positions, curr + 1)
+  defp gradient_descent(cost_fn, positions, min, max, curr) do
+    cost0 = cost_fn.(positions, curr - 1)
+    cost1 = cost_fn.(positions, curr)
+    cost2 = cost_fn.(positions, curr + 1)
 
     case Enum.min([cost0, cost1, cost2]) do
-      ^cost0 -> gradient_descent(positions, min, max, curr - 1)
+      ^cost0 -> gradient_descent(cost_fn, positions, min, max, curr - 1)
       ^cost1 -> cost1
-      ^cost2 -> gradient_descent(positions, min, max, curr + 1)
+      ^cost2 -> gradient_descent(cost_fn, positions, min, max, curr + 1)
     end
   end
 
-  def cost(positions, target) do
+  def flat_cost(positions, target) do
     positions
     |> Enum.map(&abs(target - &1))
     |> Enum.sum()
+  end
+
+  def triangle_cost(positions, target) do
+    positions
+    |> Enum.map(&abs(target - &1))
+    |> Enum.map(&triangle_number/1)
+    |> Enum.sum()
+  end
+
+  def triangle_number(0) do
+    0
+  end
+
+  def triangle_number(n) when n > 0 do
+    n + triangle_number(n - 1)
   end
 end
--- day_7/test/day7_test.exs
+++ day_7/test/day7_test.exs
@@ -32,4 +32,25 @@ defmodule Day7Test do
       assert Day7.part_1(input) == 2
     end
   end
+
+  describe "part_2" do
+    test "example 1" do
+      input = [16, 1, 2, 0, 4, 2, 7, 1, 2, 14]
+      assert Day7.part_2(input) == 168
+    end
+  end
+
+  describe "triangle_cost" do
+    test "empty" do
+      assert Day7.triangle_cost([], 1) == 0
+    end
+
+    test "one submarine moves one step" do
+      assert Day7.triangle_cost([0], 1) == 1
+    end
+
+    test "one submarine moves two steps" do
+      assert Day7.triangle_cost([0], 2) == 3
+    end
+  end
 end
