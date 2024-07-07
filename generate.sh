a37b31f30b538187538fd39fa0d8331d470bfabd
Roy van de Water <roy.v.water@gmail.com>
2024-07-07 09:41:40 -0400

    Add trialing commas to template

--- generate.sh
+++ generate.sh
@@ -11,5 +11,9 @@ main(){
   cp -r template "day_$num" \
   && rename -e "s/00/$num/" "day_$num"/**/* \
   && find "day_$num" -type file | xargs sed -i '' "s/00/$num/"
+  && pushd "day_$num"
+  && mix deps.get
+  && mix compile
+  && pushd
 }
 main "$@"
